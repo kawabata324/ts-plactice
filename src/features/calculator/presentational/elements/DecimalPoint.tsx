3e0a5205221aaec8b9aa2298b3f98b2b890a5a57
Kawabata Haruki <88492095+kawabata324@users.noreply.github.com>
2023-04-27 12:15:54 +0900

    :art: buttonの作成 (#1)
    * :art: buttonの作成
    
    * :art: zero button以外の作成

---
+++ src/features/calculator/presentational/elements/DecimalPoint.tsx
@@ -0,0 +1,3 @@
+import { FC } from "react";
+
+export const DecimalPoint: FC = () => <span className="text-5xl text-white">.</span>;
---
+++ src/stories/calcularor/PrimaryButton.stories.tsx
@@ -0,0 +1,40 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import { PrimaryButton } from "@/features/calculator/presentational/button/PrimaryButton";
+import { IconNumber1, IconPlus, IconPlusMinus } from "@tabler/icons-react";
+import { DecimalPoint } from "@/features/calculator/presentational/elements/DecimalPoint";
+
+export default {
+  title: "features/calculator/presentational/PrimaryButton",
+  component: PrimaryButton,
+} satisfies Meta<typeof PrimaryButton>;
+
+type Story = StoryObj<typeof PrimaryButton>;
+
+export const Orange: Story = {
+  args: {
+    children: <IconPlus color="white" />,
+    bgColor: "bg-orange-500",
+  },
+};
+
+export const Gray: Story = {
+  args: {
+    children: <IconNumber1 color="white" />,
+    bgColor: "bg-gray-700",
+  },
+};
+
+export const Gray2: Story = {
+  args: {
+    children: <DecimalPoint />,
+    bgColor: "bg-gray-700",
+  },
+};
+
+export const LightGray: Story = {
+  args: {
+    children: <IconPlusMinus color="black" />,
+    bgColor: "bg-gray-300",
+  },
+};
