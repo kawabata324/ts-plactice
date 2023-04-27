4e2af9956f3fa352db71442e4a0597004f06c14d
kawabata324 <kawabataharuki48@gmail.com>
2023-04-27 12:50:26 +0900

    :art: zero buttonの作成

---
+++ src/stories/calcularor/ZeroButton.stories.tsx
@@ -0,0 +1,14 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import { ZeroButton } from "@/features/calculator/presentational/buttons/ZeroButton";
+
+export default {
+  title: "features/calculator/presentational/buttons/ZeroButton",
+  component: ZeroButton,
+} satisfies Meta<typeof ZeroButton>;
+
+type Story = StoryObj<typeof ZeroButton>;
+
+export const Primary: Story = {
+  args: {},
+};
