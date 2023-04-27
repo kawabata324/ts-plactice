ef024a928a026f510183b6ea6f0c898a96cbf0f8
kawabata324 <kawabataharuki48@gmail.com>
2023-04-27 12:49:14 +0900

    :art: あくまで拡張するようにした

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    borderRadius: {
-      half: "50%",
-    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      borderRadius: {
+        half: "50%",
       },
     },
   },
