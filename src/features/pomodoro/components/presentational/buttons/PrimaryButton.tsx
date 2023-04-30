0e42ccfd0c3c9bfc5eafab285b216f9a94418970
kawabata324 <kawabataharuki48@gmail.com>
2023-04-30 19:24:28 +0900

    :art: primaryButtonの作成

--- src/features/pomodoro/components/presentational/buttons/PrimaryButton.tsx
+++ src/features/pomodoro/components/presentational/buttons/PrimaryButton.tsx
@@ -1,14 +1,20 @@
 import { FC } from "react";
+import { overrideTailwindClasses } from "tailwind-override";
 
 type Props = {
   onClick: () => void;
   label: string;
+  disabled: boolean;
+  className?: string;
 };
 
-export const PrimaryButton: FC<Props> = ({ onClick, label }) => {
+export const PrimaryButton: FC<Props> = ({ onClick, label, disabled, className }) => {
   return (
     <button
-      className="px-12 py-4 flex justify-center items-center bg-white rounded-md shadow-2xl"
+      disabled={disabled}
+      className={overrideTailwindClasses(
+        `px-12 py-4 flex justify-center items-center text-white rounded-md shadow-2xl ${className}`
+      )}
       onClick={() => onClick()}
     >
       {label}
--- src/stories/pomodoro/buttons/PrimaryButton.stories.tsx
+++ src/stories/pomodoro/buttons/PrimaryButton.stories.tsx
@@ -16,6 +16,16 @@ type Story = StoryObj<typeof PrimaryButton>;
 
 export const Primary: Story = {
   args: {
+    className: "bg-orange-500",
     label: "集中スタート",
+    disabled: false,
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    className: "bg-gray-500",
+    label: "リセット",
+    disabled: true,
   },
 };
